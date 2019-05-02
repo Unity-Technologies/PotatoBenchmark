15f55754fe65b1c765a3853b860dce95196d9341
Felipe Lira <felipedrl@gmail.com>
2019-05-02 16:57:31 +0200

    updated test to 2019.2+

--- Assets/Tests/Runtime/RuntimePerformanceTests.cs
+++ Assets/Tests/Runtime/RuntimePerformanceTests.cs
@@ -3,12 +3,13 @@ using Unity.PerformanceTesting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Rendering.LWRP;
+using UnityEngine.TestTools;
 
 public class RuntimePerformanceTests
 {
     private static readonly int warmupFrames = 10;
     private static readonly int measureFrames = 200;
-    public const string version = "0.2.0";
+    public const string version = "0.3.0";
 
     public static readonly string[] sceneNames = 
     {   
@@ -27,98 +28,98 @@ public class RuntimePerformanceTests
         Builtin,
     };
 
-    [PerformanceUnityTest]
+    [UnityTest, Performance]
     [Version(version)]
     public IEnumerator _001_FillRate_LitShader_DirectionalLight()
     {
         yield return RunTestScene(PipelineDefine.LWRP, 0);
     }
 
-    [PerformanceUnityTest]
+    [UnityTest, Performance]
     [Version(version)]
     public IEnumerator _002_FillRate_LitShader_MultipleLights()
     {
         yield return RunTestScene(PipelineDefine.LWRP, 1);
     }
 
-    [PerformanceUnityTest]
+    [UnityTest, Performance]
     [Version(version)]
     public IEnumerator _003_RealtimeShadows()
     {
         yield return RunTestScene(PipelineDefine.LWRP, 2);
     }
 
-    [PerformanceUnityTest]
+    [UnityTest, Performance]
     [Version(version)]
     public IEnumerator _004_RealtimeCascadedShadows()
     {
         yield return RunTestScene(PipelineDefine.LWRP, 3);
     }
 
-    [PerformanceUnityTest]
+    [UnityTest, Performance]
     [Version(version)]
     public IEnumerator _005_Batching()
     {
         yield return RunTestScene(PipelineDefine.LWRP, 4);
     }
     
-    [PerformanceUnityTest]
+    [UnityTest, Performance]
     [Version(version)]
     public IEnumerator _007_Bandwidth()
     {
         yield return RunTestScene(PipelineDefine.LWRP, 5);
     }
     
-    [PerformanceUnityTest]
+    [UnityTest, Performance]
     [Version(version)]
     public IEnumerator _008_FillRate_Terrain()
     {
         yield return RunTestScene(PipelineDefine.LWRP, 6);
     }
 
-    [PerformanceUnityTest]
+    [UnityTest, Performance]
     [Version(version)]
     public IEnumerator _101_FillRate_LitShader_DirectionalLight()
     {
         yield return RunTestScene(PipelineDefine.Builtin, 0);
     }
 
-    [PerformanceUnityTest]
+    [UnityTest, Performance]
     [Version(version)]
     public IEnumerator _102_FillRate_LitShader_MultipleLights()
     {
         yield return RunTestScene(PipelineDefine.Builtin, 1);
     }
 
-    [PerformanceUnityTest]
+    [UnityTest, Performance]
     [Version(version)]
     public IEnumerator _103_RealtimeShadows()
     {
         yield return RunTestScene(PipelineDefine.Builtin, 2);
     }
 
-    [PerformanceUnityTest]
+    [UnityTest, Performance]
     [Version(version)]
     public IEnumerator _104_RealtimeCascadedShadows()
     {
         yield return RunTestScene(PipelineDefine.Builtin, 3);
     }
 
-    [PerformanceUnityTest]
+    [UnityTest, Performance]
     [Version(version)]
     public IEnumerator _105_Batching()
     {
         yield return RunTestScene(PipelineDefine.Builtin, 4);
     }
     
-    [PerformanceUnityTest]
+    [UnityTest, Performance]
     [Version(version)]
     public IEnumerator _107_Bandwidth()
     {
         yield return RunTestScene(PipelineDefine.Builtin, 5);
     }
     
-    [PerformanceUnityTest]
+    [UnityTest, Performance]
     [Version(version)]
     public IEnumerator _108_FillRate_Terrain()
     {
