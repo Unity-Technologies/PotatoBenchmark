b588e9c90c912c1f8d681435156e90bb76bfd64a
Felipe Lira <felipedrl@gmail.com>
2019-07-09 15:56:09 +0200

    upgraded project to universal rp.

--- Assets/Scripts/ConfigureShadowCascades.cs
+++ Assets/Scripts/ConfigureShadowCascades.cs
@@ -9,9 +9,9 @@ public class ConfigureShadowCascades : MonoBehaviour
 
     public void Awake()
     {
-        LightweightRenderPipelineAsset asset = GraphicsSettings.renderPipelineAsset as LightweightRenderPipelineAsset;
+        UnityEngine.Rendering.Universal.UniversalRenderPipelineAsset asset = GraphicsSettings.renderPipelineAsset as UnityEngine.Rendering.Universal.UniversalRenderPipelineAsset;
         if (asset != null)
-            asset.shadowCascadeOption = (m_EnableCascades) ? ShadowCascadesOption.FourCascades : ShadowCascadesOption.NoCascades;
+            asset.shadowCascadeOption = (m_EnableCascades) ? UnityEngine.Rendering.Universal.ShadowCascadesOption.FourCascades : UnityEngine.Rendering.Universal.ShadowCascadesOption.NoCascades;
         QualitySettings.shadowCascades = (m_EnableCascades) ? 4 : 1;
     }
 }
